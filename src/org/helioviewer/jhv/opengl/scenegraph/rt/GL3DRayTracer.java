6e1fdf71f92eb37421ce1a100c89ade0e4a49d1e
Stefan Meier <stefan.meier@fhnw.ch>
2014-11-10 14:27:57 +0100

    Fixed raytrace in 2D-Mode #40
    Fixed raytrace in 2D-Mode #40

--- src/org/helioviewer/jhv/opengl/scenegraph/rt/GL3DRayTracer.java
+++ src/org/helioviewer/jhv/opengl/scenegraph/rt/GL3DRayTracer.java
@@ -1,7 +1,12 @@
 package org.helioviewer.jhv.opengl.scenegraph.rt;
 
+import java.awt.Dimension;
+
+import org.helioviewer.jhv.gui.GuiState3DWCS;
 import org.helioviewer.jhv.opengl.camera.GL3DCamera;
 import org.helioviewer.jhv.opengl.scenegraph.GL3DNode;
+import org.helioviewer.jhv.opengl.scenegraph.GL3DState;
+import org.helioviewer.jhv.opengl.scenegraph.GL3DState.VISUAL_TYPE;
 import org.helioviewer.jhv.opengl.scenegraph.math.GL3DMat4d;
 import org.helioviewer.jhv.opengl.scenegraph.math.GL3DVec3d;
 
@@ -16,75 +21,87 @@ import org.helioviewer.jhv.opengl.scenegraph.math.GL3DVec3d;
  */
 public class GL3DRayTracer {
 
-    private GL3DNode sceneRoot;
-    private GL3DCamera camera;
+	private GL3DNode sceneRoot;
+	private GL3DCamera camera;
 
-    double hh;
-    double hw;
-    double pixelSize;
+	double hh;
+	double hw;
+	double pixelSize;
 
-    public GL3DRayTracer(GL3DNode sceneRoot, GL3DCamera camera) {
-        this.sceneRoot = sceneRoot;
-        this.camera = camera;
-        hh = Math.tan(Math.toRadians(camera.getFOV() / 2)) * camera.getClipNear();
-        hw = hh * camera.getAspect();
-        pixelSize = hw / camera.getWidth() * 2;
-    }
+	public GL3DRayTracer(GL3DNode sceneRoot, GL3DCamera camera) {
+		this.sceneRoot = sceneRoot;
+		this.camera = camera;
+		hh = Math.tan(Math.toRadians(camera.getFOV() / 2))
+				* camera.getClipNear();
+		hw = hh * camera.getAspect();
+		pixelSize = hw / camera.getWidth() * 2;
+	}
 
-    public synchronized GL3DRay castCenter() {
-        GL3DRay ray = createCenterRay(this.camera);
+	public synchronized GL3DRay castCenter() {
+		GL3DRay ray = createCenterRay(this.camera);
 
-        // isOutside flag set to true if the ray hit no object in the scene
-        ray.isOutside = !this.sceneRoot.hit(ray);
-        return ray;
-    }
+		// isOutside flag set to true if the ray hit no object in the scene
+		ray.isOutside = !this.sceneRoot.hit(ray);
+		return ray;
+	}
 
-    public synchronized GL3DRay cast(int pixelX, int pixelY) {
-    	GL3DRay ray = createPrimaryRay(this.camera, pixelX, pixelY);
+	public synchronized GL3DRay cast(int pixelX, int pixelY) {
+		GL3DRay ray = createPrimaryRay(this.camera, pixelX, pixelY);
 
-        // isOutside flag set to true if the ray hit no object in the scene
-        ray.isOutside = !this.sceneRoot.hit(ray);
-        return ray;
-    }
+		// isOutside flag set to true if the ray hit no object in the scene
+		ray.isOutside = !this.sceneRoot.hit(ray);
+		return ray;
+	}
 
-    private GL3DRay createCenterRay(GL3DCamera camera) {
-        GL3DMat4d VM = camera.getVM();
-        GL3DVec3d LA = new GL3DVec3d();
-        GL3DVec3d LR = new GL3DVec3d();
-        GL3DVec3d LU = new GL3DVec3d();
-        GL3DVec3d EYE = new GL3DVec3d();
+	private GL3DRay createCenterRay(GL3DCamera camera) {
+		GL3DMat4d VM = camera.getVM();
+		GL3DVec3d LA = new GL3DVec3d();
+		GL3DVec3d LR = new GL3DVec3d();
+		GL3DVec3d LU = new GL3DVec3d();
+		GL3DVec3d EYE = new GL3DVec3d();
 
-        VM.readLookAt(EYE, LA, LU, LR);
-        LA.normalize();
+		VM.readLookAt(EYE, LA, LU, LR);
+		LA.normalize();
 
-        GL3DVec3d C = LA.multiply(camera.getClipNear());
+		GL3DVec3d C = LA.multiply(camera.getClipNear());
 
-        GL3DRay ray = GL3DRay.createPrimaryRay(EYE, C);
-        return ray;
-    }
+		GL3DRay ray = GL3DRay.createPrimaryRay(EYE, C);
+		return ray;
+	}
 
-    private GL3DRay createPrimaryRay(GL3DCamera camera, int x, int y) {
-        GL3DMat4d VM = camera.getVM();
-        GL3DVec3d LA = new GL3DVec3d();
-        GL3DVec3d LR = new GL3DVec3d();
-        GL3DVec3d LU = new GL3DVec3d();
-        GL3DVec3d EYE = new GL3DVec3d();
-        VM.readLookAt(EYE, LA, LU, LR);
-        LA.normalize();
-        LU.normalize();
-        LR.normalize();
-        GL3DVec3d C = LA.multiply(camera.getClipNear());
-        GL3DVec3d TL = C.subtract(LR.copy().multiply(hw)).add(LU.copy().multiply(hh));
-
-        GL3DVec3d dir = TL.copy().add(LR.copy().multiply(x).subtract(LU.copy().multiply(y)).multiply(pixelSize));
-        // dir = VM.copy().mat3().inverse().multiply(dir);
-        // EYE = VM.copy().inverse().multiply(EYE);
-
-        // GL3DMat4d VM_Inv = VM.inverse();
-        // dir = VM_Inv.mat3().multiply(dir);
-        // EYE = VM_Inv.multiply(EYE);
-        GL3DRay ray = GL3DRay.createPrimaryRay(EYE, dir);
-        return ray;
-    }
+	private GL3DRay createPrimaryRay(GL3DCamera camera, int x, int y) {
+		GL3DRay ray;
+		if (GL3DState.get().getState() == VISUAL_TYPE.MODE_3D) {
+			GL3DMat4d VM = camera.getVM();
+			GL3DVec3d LA = new GL3DVec3d();
+			GL3DVec3d LR = new GL3DVec3d();
+			GL3DVec3d LU = new GL3DVec3d();
+			GL3DVec3d EYE = new GL3DVec3d();
+			VM.readLookAt(EYE, LA, LU, LR);
+			LA.normalize();
+			LU.normalize();
+			LR.normalize();
+			GL3DVec3d C = LA.multiply(camera.getClipNear());
+			GL3DVec3d TL = C.subtract(LR.copy().multiply(hw)).add(
+					LU.copy().multiply(hh));
+			GL3DVec3d dir = TL.copy().add(
+					LR.copy().multiply(x).subtract(LU.copy().multiply(y))
+							.multiply(pixelSize));
+			ray = GL3DRay.createPrimaryRay(EYE, dir);
+		} else {
+			GL3DVec3d dir = new GL3DVec3d(0, 0, -1.0);
+			GL3DVec3d eye = new GL3DVec3d(camera.getTranslation().copy()
+					.negate());
+			double height = camera.getZTranslation()
+					* Math.tanh(Math.toRadians(camera.getFOV()));
+			Dimension dimension = GuiState3DWCS.mainComponentView
+					.getCanavasSize();
+			double unitPerPixel = height / dimension.getHeight();
+			eye.x += unitPerPixel * (x - dimension.getWidth() / 2.0);
+			eye.y += unitPerPixel * (y - dimension.getHeight() / 2.0);
+			ray = GL3DRay.createPrimaryRay(eye, dir);
+		}
+		return ray;
+	}
 
 }
