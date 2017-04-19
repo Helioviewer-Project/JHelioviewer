69e487af815f78a0f57a0005c7ce4f2c402a7057
Manuel Serquet <manuelserquet@hotmail.com>
2017-04-19 14:28:54 +0200

    added instancing of Hub if none is running

--- src/org/helioviewer/jhv/plugins/samp/SampClient.java
+++ src/org/helioviewer/jhv/plugins/samp/SampClient.java
@@ -1,5 +1,6 @@
 package org.helioviewer.jhv.plugins.samp;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -8,6 +9,8 @@ import org.astrogrid.samp.Metadata;
 import org.astrogrid.samp.client.ClientProfile;
 import org.astrogrid.samp.client.HubConnector;
 import org.astrogrid.samp.client.SampException;
+import org.astrogrid.samp.hub.Hub;
+import org.astrogrid.samp.hub.HubServiceMode;
 import org.helioviewer.jhv.base.math.MathUtils;
 import org.helioviewer.jhv.layers.ImageLayer;
 import org.helioviewer.jhv.layers.Layers;
@@ -25,6 +28,22 @@ public class SampClient extends HubConnector
 	{
 		super(_profile);
 		Metadata meta = new Metadata();
+
+		
+		Hub[] runningHubs = Hub.getRunningHubs();
+		if(runningHubs.length == 0)
+		{
+			try
+			{
+				Hub.checkExternalHubAvailability();
+				Hub.runExternalHub(HubServiceMode.CLIENT_GUI);
+			}
+			catch (IOException _e1)
+			{
+				// TODO Auto-generated catch block
+				_e1.printStackTrace();
+			}
+		}
 		
 		// TODO: name /  description
 		meta.setName("JHelioviewer");
